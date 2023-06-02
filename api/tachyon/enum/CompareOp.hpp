04b1d11d15f43a2cc060ab970a73fb842d912990
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-06-02 07:56:31 +0000

    Doxygen

--- api/tachyon/enum/CompareOp.hpp
+++ api/tachyon/enum/CompareOp.hpp
@@ -10,14 +10,48 @@
 #include <meta/InfoBinder.hpp>
 
 namespace yq::tachyon {
+    
+    /*! \brief Comparison operator
+    
+        This is the comparison operator enumeration (used by vulkan).
+    *//*
+        From this table... easy to see the bit pattern
+        
+        never           000
+        less            001
+        equal           010
+        less_equal      011
+        greater         100
+        not equal       101
+        greater equal   110
+        always          111
+        
+        (ie, the comparison on the GPU comes to three status bits, 
+            and this is the match/no-match state)
+    */
     YQ_ENUM(CompareOp, , 
+        //! Always fail
         Never           = 0,
+        
+        //! Less than
         Less            = 1,
+        
+        //! Equality
         Equal           = 2,
+        
+        //! Less Or equal
         LessOrEqual     = 3,
+        
+        //! Greater
         Greater         = 4,
+        
+        //! Not equal
         NotEqual        = 5,
+        
+        //! Greater or equal
         GreaterOrEqual  = 6,
+        
+        //! Always
         Always          = 7
     )
 }

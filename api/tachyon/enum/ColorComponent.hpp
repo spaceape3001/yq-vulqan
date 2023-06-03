73d118a36322ff1701379db64f20bcbf6aaa91ce
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-06-03 05:50:26 +0000

    Doxygen

--- api/tachyon/enum/ColorComponent.hpp
+++ api/tachyon/enum/ColorComponent.hpp
@@ -11,13 +11,30 @@
 #include <meta/InfoBinder.hpp>
 
 namespace yq::tachyon {
+    /*! \brief Color Component
+    
+        Used in swizzling, this specifies color components.  
+        Values are based on vulkan specification.
+    */
     YQ_ENUM(ColorComponent, , 
+    
+        //! Red
         Red     = 0,
+        
+        //! Green
         Green   = 1,
+        
+        //! Blue
         Blue    = 2,
+        
+        //! Alpha
         Alpha   = 3
     )
     
+    /*! \brief Color Component Flags
+    
+        Useful when needing to bit flag color components
+    */
     using ColorComponentFlags   = Flag<ColorComponent>;
 }
 

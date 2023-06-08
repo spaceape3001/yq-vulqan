0b89617b3c86f0aec59f8509592eeb619429dff3
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-06-08 07:46:49 +0000

    Doxygen

--- api/tachyon/enum/FrontFace.hpp
+++ api/tachyon/enum/FrontFace.hpp
@@ -10,6 +10,12 @@
 #include <meta/InfoBinder.hpp>
 
 namespace yq::tachyon {
+    /*! \brief Which face is front
+    
+        This indicates to the GPU which face of a triangle is "front side" for rendering or not.
+        Combined with the cull mode, this can speed up rendering by telling the pipeline to 
+        skip a face based on this orientation.
+    */
     YQ_ENUM(FrontFace, , 
         CounterClockwise    = 0,
         Clockwise = 1

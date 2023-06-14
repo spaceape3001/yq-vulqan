c499d6be23b24cc5a2d59488e5b46dd086d98f52
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-06-14 06:16:31 +0000

    Doxygen

--- api/tachyon/enum/ModifierKey.hpp
+++ api/tachyon/enum/ModifierKey.hpp
@@ -11,6 +11,10 @@
 #include <meta/InfoBinder.hpp>
 
 namespace yq::tachyon {
+    /*! \brief Modifier Keys
+    
+        These are obviously used in UI framework, expected/supported modifier keys
+    */
     YQ_ENUM(ModifierKey, , 
         Shift       = 0,
         Control,
@@ -19,6 +23,7 @@ namespace yq::tachyon {
         CapsLock,
         NumLock
     )
+    //! Bit mask of modifier keys
     using ModifierKeys  = Flag<ModifierKey,uint8_t>;
 }
 YQ_TYPE_DECLARE(yq::tachyon::ModifierKey)

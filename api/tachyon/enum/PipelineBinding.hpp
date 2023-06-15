7a589b3758e25ffc2f40a16a807794e81d622693
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-06-15 07:51:39 +0000

    Doxygen & Bug Fix

--- api/tachyon/enum/PipelineBinding.hpp
+++ api/tachyon/enum/PipelineBinding.hpp
@@ -11,9 +11,13 @@
 #include <meta/InfoBinder.hpp>
 
 namespace yq::tachyon {
+    /*! \brief Binding point for this pipeline
+    
+        Basically, the type of pipeline 
+    */
     YQ_ENUM(PipelineBinding, , 
         Graphics = 0,
-        Compute = 0,
+        Compute = 1,
         RayTracing = 1000165000,
         SubpassShading = 1000369003
     )

dc4af33447ba3cd45a5296f8fdb510a72f719cc9
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-06-16 06:26:19 +0000

    Doxygen

--- api/tachyon/enum/PolygonMode.hpp
+++ api/tachyon/enum/PolygonMode.hpp
@@ -10,6 +10,12 @@
 #include <meta/InfoBinder.hpp>
 
 namespace yq::tachyon {
+
+    /*! \brief Polygon mode for pipelines
+    
+        This is the polygon mode for pipelines.  
+        Set to Line to get wireframe effect.
+    */
     YQ_ENUM(PolygonMode, , 
         Fill = 0,
         Line = 1,

97685b05d8cd0a96c64c324447f1630723d6c417
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-06-18 06:42:25 +0000

    Doxygen

--- api/tachyon/enum/PushConfigType.hpp
+++ api/tachyon/enum/PushConfigType.hpp
@@ -9,10 +9,19 @@
 #include <cstdint>
 
 namespace yq::tachyon {
+    /*! \brief Type of push configuration
+    
+        This is the general type of push configuration a pipeline uses;
+        two are explicitly called out for better runtime performance
+    */
     enum PushConfigType : uint8_t {
+        //! No push data
         None        = 0,
+        //! Full model->screen matrix requested
         Full,
+        //! Partial world->screen matrix requested
         View,
+        //! Custom push data
         Custom
     };
 }

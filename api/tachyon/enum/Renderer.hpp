77e275774696c89c4e367103f53e7c0331923e8d
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-06-19 08:36:19 +0000

    Doxygen

--- api/tachyon/enum/Renderer.hpp
+++ api/tachyon/enum/Renderer.hpp
@@ -9,11 +9,16 @@
 #include <basic/Flags.hpp>
 
 namespace yq::tachyon {
+    /*! \brief Renderer
+    
+        Hint to renderers
+    */
     enum class Renderer : uint8_t {
         Vulkan  = 0,
         ImGui
     };
     
+    //! Bitmask of renderers.
     using RendererFlags = Flags<Renderer,uint8_t>;
 }
 

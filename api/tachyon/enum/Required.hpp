ccbacf83910e4602b040102a0d1db2d6c5894ca6
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-06-20 06:57:50 +0000

    Doxygen

--- api/tachyon/enum/Required.hpp
+++ api/tachyon/enum/Required.hpp
@@ -9,9 +9,16 @@
 #include <cstdint>
 
 namespace yq::tachyon {
+    /*! \brief Enumeration for required values
+    
+        Used when a tristate is needed for require-values
+    */
     enum class Required : uint8_t {
+        //! Feature not required/used
         NO = 0,
+        //! Feature used, but not required
         OPTIONAL,
+        //! Feature used and is required
         YES
     };
 }

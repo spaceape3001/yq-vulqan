472221f5d81b4240903e0db240903b7bda843176
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-06-21 07:41:46 +0000

    Doxygen

--- api/tachyon/enum/SamplerAddressMode.hpp
+++ api/tachyon/enum/SamplerAddressMode.hpp
@@ -10,11 +10,26 @@
 #include <meta/InfoBinder.hpp>
 
 namespace yq::tachyon {
+    /*! \brief Sampler's out-of-bounds conditions
+    
+        How the sampler deals with u,v < 0 and/or u,v > 1
+    */
     YQ_ENUM(SamplerAddressMode, ,
+    
+        //! Periodic
         Repeat  = 0,
+        
+        //! Mirrored but periodic
         MirroredRepeat = 1,
+        
+        //! Clamps to the edge
         ClampToEdge = 2,
+        
+        //! Clamps to the border
         ClampToBorder = 3,
+        
+        //! Mirror clamp to edge
+        //! \note Needs extension to work
         MirrorClampToEdge = 4
     )
 }

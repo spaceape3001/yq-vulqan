c38447e59a02a210e3d19c871e66c0f200f1b01a
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-06-23 06:44:40 +0000

    Doxygen

--- api/tachyon/enum/SamplerFilter.hpp
+++ api/tachyon/enum/SamplerFilter.hpp
@@ -10,9 +10,17 @@
 #include <meta/InfoBinder.hpp>
 
 namespace yq::tachyon {
+
+    /*! \brief Sampler filter
+    
+        This controls the sampler filter
+    */
     YQ_ENUM(SamplerFilter, ,
+        //! Nearest pixel
         Nearest     = 0,
+        //! Linear interpolation?
         Linear      = 1,
+        //! Cubic interpolation?
         Cubic       = 1000015000
     )
 }

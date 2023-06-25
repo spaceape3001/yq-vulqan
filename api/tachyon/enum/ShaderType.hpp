510c0bc85e1806224783844ebaeacbfc120c34c2
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-06-25 06:23:27 +0000

    Doxygen

--- api/tachyon/enum/ShaderType.hpp
+++ api/tachyon/enum/ShaderType.hpp
@@ -10,16 +10,28 @@
 #include <meta/InfoBinder.hpp>
 
 namespace yq::tachyon {
+    /*! \brief Shader Type
+    
+        This is the type of shader (supported)
+    */
     YQ_ENUM(ShaderType, ,
+        //! Unknown shader type
         UNKNOWN,
+        //! Vertex shader
         VERT,
+        //! Tessellation Evaluation shader
         TESE,
+        //! Tessellation Control shader
         TESC,
+        //! Fragment shader
         FRAG,
+        //! Geometry shader
         GEOM,
+        //! Compute shader
         COMP
     )
     
+    //! Mask of shader types
     using ShaderTypeFlags   = Flag<ShaderType>;
 }
 

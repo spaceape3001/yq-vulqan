44e896a87454cc840491d1b192330493ee8109e6
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-06-26 06:14:52 +0000

    Doxygen

--- api/tachyon/enum/StencilOp.hpp
+++ api/tachyon/enum/StencilOp.hpp
@@ -10,14 +10,32 @@
 #include <meta/InfoBinder.hpp>
 
 namespace yq::tachyon {
+
+    //! Stencil operation
     YQ_ENUM(StencilOp, ,
+    
+        //! Keep
         Keep = 0,
+        
+        //! Zero
         Zero = 1,
+        
+        //! Replace
         Replace = 2,
+        
+        //! Increment and clamp
         IncrementAndClamp = 3,
+
+        //! Decrement and clamp
         DecrementAndClamp = 4,
+        
+        //! Invert
         Invert = 5,
+        
+        //! Increment and wrap
         IncrementAndWrap = 6,
+        
+        //! Decrement and wrap
         DecrementAndWrap = 7
     )
 }

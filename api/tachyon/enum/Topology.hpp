393a0e05c9ef73f02c60912948aa99cb7ea7bb82
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-06-29 06:08:17 +0000

    Doxygen

--- api/tachyon/enum/Topology.hpp
+++ api/tachyon/enum/Topology.hpp
@@ -10,17 +10,44 @@
 #include <meta/InfoBinder.hpp>
 
 namespace yq::tachyon {
+
+    /*! \brief Topology 
+    
+        This is the topology of an object going into the pipeline to be rendered.
+    */
     YQ_ENUM(Topology, , 
+        //! List of points
         PointList = 0,
+        
+        //! Line list
         LineList = 1,
+        
+        //! Line strip
         LineStrip = 2,
+        
+        //! List of triangles
         TriangleList = 3,
+        
+        //! Triangle strip
         TriangleStrip = 4,
+        
+        //! Triangle fan
         TriangleFan = 5,
+        
+        //! Line list with adjacency
         LineListWithAdjacency = 6,
+        
+        //! Line strip with adjacency
         LineStripWithAdjacency = 7,
+        
+        //! Triangle list with adjacency
         TriangleListWithAdjacency = 8,
+        
+        //! Triangle strip with adjacency
         TriangleStripWithAdjacency = 9,
+        
+        //! Patch list
+        //! (note, use this if you're doing GPU tessellation)
         PatchList = 10
     )
 }

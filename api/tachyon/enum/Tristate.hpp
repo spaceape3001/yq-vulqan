1ccab7a941122eae3fda5bf1ee404e34917fe379
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-06-30 07:47:13 +0000

    Doxygen

--- api/tachyon/enum/Tristate.hpp
+++ api/tachyon/enum/Tristate.hpp
@@ -10,11 +10,26 @@
 #include <meta/InfoBinder.hpp>
 
 namespace yq::tachyon {
+
+    /*! \brief Tristate enumeration
+    
+        This tristate allows for overriding or taking the prevailing option
+    */
     YQ_ENUM(Tristate, ,
+        
+        //! Default is a "maybe", which means inherit and/or optional
         MAYBE       = 1,
+        
+        //! Assert a NO on this state, override the inheritance
         NO          = 0,
+        
+        //! Assert a YES on this state, override the inheritance
         YES         = 2,
+        
+        //! Synonym for "maybe"
         INHERIT     = MAYBE,
+
+        //! Synonym for "maybe"
         OPTIONAL    = MAYBE
     );
 }

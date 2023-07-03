b0aafbc7af32ccc6327dbcb7a30d3aeae0a30d92
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-07-03 08:09:17 +0000

    Doxygen

--- api/tachyon/host/VideoMode.hpp
+++ api/tachyon/host/VideoMode.hpp
@@ -11,12 +11,28 @@
 #include <math/shape/Size2.hpp>
 
 namespace yq::tachyon {
+    /*! \brief Video mode information
+    
+        Video mode information about what a monitor supports
+    */
     struct VideoMode {
+    
+        //! Size of the video mode
         Size2I      size            = {};
+        
+        //! Bit counts for the monitor
         RGB3I       bits            = {};
+        
+        //! Refresh rate
         int         refresh_rate    = 0;
+        
+        //! Default constructor
         VideoMode(){}
+        
+        //! Construction from GLFWvidmode
         VideoMode(const GLFWvidmode&);
+        
+        //! Equality operator (defaulted)
         constexpr bool    operator==(const VideoMode&) const noexcept = default;
     };
 }

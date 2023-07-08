cdf5aade1f4b9bef6cb58f4c80e6612c456c2572
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-07-08 07:47:37 +0000

    Doxygen

--- api/tachyon/asset/render/Triangle.hpp
+++ api/tachyon/asset/render/Triangle.hpp
@@ -12,6 +12,11 @@
 #include <tachyon/viz/VBO.hpp>
 
 namespace yq::tachyon {
+
+    /*! \brief Triangle
+    
+        This is a 2D triangle -- should be on a 2D render, but it's 3D.
+    */
     class Triangle : public Render3D {
         YQ_OBJECT_DECLARE(Triangle, Render3D)
     public:

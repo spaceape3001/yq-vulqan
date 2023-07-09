df9e4e3a919bdf20240a55ab725fbe69d04cdeac
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-07-09 08:05:56 +0000

    Doxygen

--- api/tachyon/scene/Camera.hpp
+++ api/tachyon/scene/Camera.hpp
@@ -18,12 +18,18 @@ namespace yq::tachyon {
 
     class Camera;
     
+    /*! \brief Camera Information
+    
+        Information for cameras.
+    */
     class CameraInfo : public ObjectInfo {
     public:
         template <typename C> struct Writer;
 
+        //! Gets all camera informations
         static const std::vector<const CameraInfo*>&    all();
         
+        //! Standard constructor for the camera information
         CameraInfo(std::string_view, ObjectInfo&, const std::source_location& sl = std::source_location::current());
     private:
         struct Repo;
@@ -41,24 +47,38 @@ namespace yq::tachyon {
         YQ_OBJECT_DECLARE(Camera, Object)
     public:    
     
+        //! Generic parameter structure
+        //! Allows us to expand parameters w/o affecting *everybody*
         struct Params;
     
         //! Returns the transform to go world -> screen space
         virtual glm::dmat4  world2screen(const Params&) const = 0;
     
+        //! Name of this camera's instance
         std::string_view    name() const { return m_name; }
+        
+        //! Getter for the property system
         const std::string&  get_name() const { return m_name; }
+        
+        //! Sets the camera's name
         void                set_name(const std::string&);
 
     protected:
+    
+        //! Default constructor
         Camera();
+        
+        //! Default destructor
         ~Camera();
         
     private:
         std::string         m_name;
     };
 
+    /*! \brief Camera parameters
+    */
     struct Camera::Params {
+        //  Dimensions of the screen
         Rectangle2D     screen;
     };
 

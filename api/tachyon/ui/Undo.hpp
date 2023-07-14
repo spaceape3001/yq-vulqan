b144e187c9d382dca085be159487b4e9ccfa54c3
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-07-14 07:34:24 +0000

    Doxygen

--- api/tachyon/ui/Undo.hpp
+++ api/tachyon/ui/Undo.hpp
@@ -10,16 +10,26 @@
 #include <string_view>
 
 namespace yq::tachyon {
+
+    //! Root undo thing
+    //!
+    //! An undo infrastructure
     class Undo {
     public:
+    
+        //! Undo whatever
         virtual void undo() {}
+        
+        //! Redo it
         virtual void redo() {}
         
         //! Allows undos to combine (ie, text and more text)
         virtual bool combine(Undo* next);
         
+        //! Label/text
         std::string_view   label() const { return m_label; }
         
+        //! Default virtual destructor
         virtual ~Undo();
         
     protected:

b3646a8d1cfa45c8f5ab6c35bc4a4b0d5602be9d
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-07-17 07:07:00 +0000

    Doxygen

--- api/tachyon/viz/BufferObject.hpp
+++ api/tachyon/viz/BufferObject.hpp
@@ -11,15 +11,19 @@
 
 namespace yq::tachyon {
 
+    //! Helper for buffer objects
+    //! Simple helper to help with managing buffer objects, and spinning their results out
     template <BufferUsage::enum_t buf>
     struct BufferObject {
         BufferCPtr      buffer;
         
+        //! Updates the memory buffer
         void    update(Memory&&mem)
         {
             buffer      = new Buffer(buf, std::move(mem));
         }
         
+        //! Assigns the buffer object
         BufferObject& operator=(const BufferObject& obj)
         {
             buffer      = obj.buffer;
@@ -32,7 +36,12 @@ namespace yq::tachyon {
         ~BufferObject(){}
     };
 
+    //! Vertex Buffer
     using VertexBuffer  = BufferObject<BufferUsage::Vertex>;
+    
+    //! Index Buffer
     using IndexBuffer   = BufferObject<BufferUsage::Index>;
+    
+    //! Uniform Buffer
     using UniformBuffer = BufferObject<BufferUsage::Uniform>;
 }

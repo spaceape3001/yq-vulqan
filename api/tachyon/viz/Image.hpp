7bba370d4e6d983a0cad4417df0b0908e08c7c36
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-07-18 06:46:03 +0000

    Doxygen

--- api/tachyon/viz/Image.hpp
+++ api/tachyon/viz/Image.hpp
@@ -23,14 +23,22 @@ namespace yq::tachyon {
         YQ_OBJECT_DECLARE(Image, Asset)
     public:
     
+        //! Cache of images
         static TypedAssetFactory<Image>&    cache();
+        
+        //! Loads an image by key
         static const Image*                 load(std::string_view);
     
+        //! Data for the image
         const Memory      memory;
+        
+        //! Information for the image
         const ImageInfo   info;
-    
+        
+        //! Constructor (takes info & data)
         Image(const ImageInfo&, Memory&&);
 
+        //! Returns the size of the memory data
         virtual size_t      data_size() const  override;
 
     private:

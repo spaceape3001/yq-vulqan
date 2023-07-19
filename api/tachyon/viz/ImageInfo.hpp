690a737a5b1eaa38eaec216525aa45a23e6bcacc
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-07-19 07:57:15 +0000

    Doxygen

--- api/tachyon/viz/ImageInfo.hpp
+++ api/tachyon/viz/ImageInfo.hpp
@@ -19,11 +19,22 @@ namespace yq::tachyon {
         Basic image information structure, can be used by pixmaps & textures
     */
     struct ImageInfo {
+        //! Size of the image
         Size4U      size        = Size4U(ONE);
+        
+        //! Format of the image
         DataFormat  format      = DataFormat::R8G8B8A8_SRGB;
+        
+        //! General dimension of image
         ImageType   type        = ImageType::Is2D;
+        
+        //! Image tiling
         ImageTiling tiling      = ImageTiling::Optimal;
+        
+        //! Number of layers in the array
         unsigned    arrayLayers = 1;
+        
+        //! Number of miplevels in the image
         unsigned    mipLevels   = 1;
     };
 

8a3effe7f5da9af59a3f25ec466c929de6948663
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-07-21 09:17:01 +0000

    Doxygen

--- api/tachyon/viz/PushData.hpp
+++ api/tachyon/viz/PushData.hpp
@@ -12,10 +12,19 @@ namespace yq::tachyon {
 
     //! Standard Push Data
     struct StdPushData {
-        glm::mat4   matrix      = {};     // either view or render... depending on enumeration
+        //! Either a model -> screen matrix, or world -> screen matrix, depending on the enumeration
+        glm::mat4   matrix      = {};
+        
+        //! Color by color gamma multipliers
         glm::vec4   gamma       = { 1., 1., 1., 1. };
+        
+        //! Primary color for object
         glm::vec4   pricolor    = {};
+        
+        //! Secondary color for object
         glm::vec4   seccolor    = {};
+        
+        //! Clock time
         float       time        = 0;
     };
 }

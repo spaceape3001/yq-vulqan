a7e1ce57a027866ed6c8f64aef15cd2552e165f2
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-07-23 09:22:21 +0000

    Doxygen

--- api/tachyon/viz/Shader.hpp
+++ api/tachyon/viz/Shader.hpp
@@ -17,20 +17,33 @@ namespace yq::tachyon {
         YQ_OBJECT_DECLARE(Shader, Asset)
     public:
     
+    
+        //! Shader cache
         static TypedAssetFactory<Shader>&  cache();
         
+        //! Loads the specified shader
         static Ref<const Shader>    load(std::string_view);
+        
+        //! Decodes the specified shader
         static Ref<const Shader>    decode(const ShaderSpec&);
+        
+        //! Gets the name to the specified shader
         static std::string_view     name(const ShaderSpec&);
         
+        //! Size of the compiled shader 
         virtual size_t      data_size() const override;
         
+        //! Shader constructor
         Shader(ShaderType, Memory&&);
 
+        //! Shader bytecode
         const Memory      payload;
+        
+        //! Shader type
         const ShaderType  type;
     
     private:
+        //! Private destructor as these are considered load-once and keep until application exit
         virtual ~Shader();
     };
 }

a934b3aa88a6244c8a2d1b6451352e9cb1a6dbdc
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-07-24 07:08:03 +0000

    Doxygen

--- api/tachyon/viz/Texture.hpp
+++ api/tachyon/viz/Texture.hpp
@@ -12,13 +12,20 @@
 
 namespace yq::tachyon {
 
+    //! Texture (image & view/sampler info)
     class Texture : public UniqueID, public RefCount {
     public:
     
+        //! Image
         const Ref<const Image>      image;
+        
+        //! View information
         const ImageViewInfo         view;
+        
+        //! Sampler info
         const SamplerInfo           sampler;
         
+        //! Default constructor
         Texture(Ref<const Image>, const ImageViewInfo&, const SamplerInfo&);
         
     private:

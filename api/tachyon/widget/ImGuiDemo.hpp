d6d05faa3713de485acfeb91f347e66b91827d9d
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-07-26 08:21:40 +0000

    Doxygen

--- api/tachyon/widget/ImGuiDemo.hpp
+++ api/tachyon/widget/ImGuiDemo.hpp
@@ -9,13 +9,20 @@
 #include <tachyon/Widget.hpp>
 
 namespace yq::tachyon {
+    //! ImGuiDemo widget
+    //!
+    //! This shows the ImGui demo as a widget
     class ImGuiDemo : public Widget {
         YQ_OBJECT_DECLARE(ImGuiDemo, Widget)
     public:
     
+        //! Default constructor
         ImGuiDemo();
+        
+        //! Default destructor
         ~ImGuiDemo();
     
+        //! Imgui render
         virtual void    imgui_(ViContext&) override;
     };
 }

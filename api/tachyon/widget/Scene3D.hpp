d791c07f1d4ad303660ebe9ad94b52f7c697c6c4
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-07-27 06:49:26 +0000

    Doxygen

--- api/tachyon/widget/Scene3D.hpp
+++ api/tachyon/widget/Scene3D.hpp
@@ -11,21 +11,37 @@
 #include <tachyon/scene/Scene.hpp>
 
 namespace yq::tachyon {
+
+    /*! \brief Widget for a scene
+    
+        Simple widget that allows for a camera & scene (render-frame)
+    */
     class Scene3D : public Widget {
         YQ_OBJECT_DECLARE(Scene3D, Widget)
     public:
     
+        //! Our perspective (ie, camera)
         Perspective     perspective;
+        
+        //! Our scene (ie, renders, lights, etc)
         Scene           scene;
     
+        //! Default constructor
         Scene3D();
+        
+        //! Default destructor
         virtual ~Scene3D();
     
+        //! Vulkan recording override
         virtual void    vulkan_(ViContext&) override;
 
+        //! Pre-record (ie, for textures, UBOS)
         virtual void    prerecord(ViContext&) override;
         
+        //! Adds a new thing to the scene (helper)
         void    add_thing(RenderedPtr);
+        
+        //! Sets the camera for us (helper)
         void    set_camera(CameraCPtr);
     };
 }

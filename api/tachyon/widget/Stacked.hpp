134043b3b5a3c825f840d449d8987a279f2e349b
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-07-28 06:36:35 +0000

    Doxygen

--- api/tachyon/widget/Stacked.hpp
+++ api/tachyon/widget/Stacked.hpp
@@ -18,15 +18,28 @@ namespace yq::tachyon {
         YQ_OBJECT_DECLARE(Stacked, Widget)
     public:
 
+        //! Active widget
         Widget*         active();
+
+        //! Active widget
         const Widget*   active() const;
+
+        //! Current index
         size_t          index() const { return m_active; }
+        
+        //! Sets the current index
         void            set_active(size_t);
         
+        //! Renders imgui data
         virtual void    imgui_(ViContext&) override;
+
+        //! Renders vulkan data
         virtual void    vulkan_(ViContext&) override;
 
+        //! Default constructor
         Stacked();
+        
+        //! Default destructor
         ~Stacked();
     
     private:

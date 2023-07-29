d68784a6dc377dd9649e8aeff5301378bea9ac1d
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-07-29 07:19:07 +0000

    Doxygen

--- api/tachyon/Widget.hpp
+++ api/tachyon/Widget.hpp
@@ -31,10 +31,30 @@ namespace yq::tachyon {
         YQ_OBJECT_DECLARE(Widget, Object)
     public:
     
+        //! Default constructor
         Widget();
+        
+        //! Default destructor
         virtual ~Widget();
 
+        /*! \brief Renders ImGui content
+        
+            This routine is called by the viewer for our widget 
+            to properly render ImGui content.  The context
+            is all the extra information that may or may
+            not be needed.  Default implementation iterates 
+            through children, calling their methods recursively.
+        */
         virtual void    imgui_(ViContext&);
+        
+        /*! \brief Renders Vulkan content
+        
+            This routine is called by the viewer for our widget to
+            properly render Vulkan content.  The context 
+            is all the extra information that may or may
+            not be needed.  Default implementation iterates
+            through children, calling their methods recursively.
+        */
         virtual void    vulkan_(ViContext&);
         
         /*! \brief Sets the parent of this widget
@@ -66,44 +86,73 @@ namespace yq::tachyon {
         */
         bool    has_parentage(const Widget* p) const;
         
+        //! Our parent widget
         Widget*         parent()  { return m_parent; }
+
+        //! Our parent widget
         const Widget*   parent() const { return m_parent; }
 
+        //! Our root widget
         Widget*         root();
+
+        //! Our root widget
         const Widget*   root() const;
         
+        //! Our viewer
         Viewer*         viewer();
+
+        //! Our viewer
         const Viewer*   viewer() const ;
 
     protected:
         friend class Viewer;
+        
+        //! Our viewer
         Viewer*                 m_viewer    = nullptr;
+        
+        //! Our parent widget
         Widget*                 m_parent    = nullptr;
+        
+        //! Our child widgets
         std::vector<Widget*>    m_children;
+        
+        //! A string ID for ImGui
         std::string             m_windowID;
         
+        //! Called before record, this is the opportunity to 
+        //! pass descriptor sets to the graphics card.
         virtual void            prerecord(ViContext&);
 
+        //! Called when a child of this widget is added
         virtual void            child_added(Widget*){}
+        
+        //! Called when a child of this widget is removed
         virtual void            child_removed(Widget*){}
     };
 
+    /*! \brief Writer of widget information
+    */
     template <typename C>
     class WidgetInfo::Writer : public ObjectInfo::Writer<C> {
     public:
+    
+        //! Constructor of widget info (this is used by derived classes and this classes other constructor)
         Writer(WidgetInfo* widgetInfo) : ObjectInfo::Writer<C>(widgetInfo), m_meta(widgetInfo)
         {
         }
         
+        //! Constructor of widget info (this is used by the writer<T>() methods)
         Writer(WidgetInfo& widgetInfo) : Writer(&widgetInfo)
         {
         }
 
+        //! Annotates that this widget will render IMGUI content
         void    imgui()
         {
             Meta::Writer::options({Flag::IMGUI});
         }
         
+        //! Annotates that this widget will render Vulkan content
         void    vulkan()
         {
             Meta::Writer::options({Flag::VULKAN});

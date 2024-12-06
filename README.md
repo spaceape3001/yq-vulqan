8ba773ba334b8962909e23309c22330faf73adee
spaceape3001 <spaceape3001@users.noreply.github.com>
2024-12-06 09:08:09 +0000

    Positions to Unity
    Unity style building is returning (practicalities)

--- README.md
+++ README.md
@@ -2,13 +2,11 @@
 
 Vulkan/Visual Engine for my Your Quill project
 
+***UPDATE*** (Dec 6) Still working on the multithreadedness, that's taking a while.
+
 ***UPDATE*** (Nov 12) ImGui works, camera works
 
 ***UPDATE*** (Nov 9) Hello Triangle builds, runs w/o crashing.  Won't interact much.  ImGui based projects (camera, demo) are inoperable.
-
-***UPDATE*** (Oct 30) Stabilized so it builds.  Sample app will launch, but won't interact, and seg-faults on "X" exit.  Still, better than before, so pushing & will polish up on dev-branch.
-
-***WARNING*** (Oct 28) Accidentally pushed an interrim commit, so master is currently busted at the moment.
 
 
 
---
+++ api/yq/tachyon/commands/commands.cpp
@@ -0,0 +1,37 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//  YOUR QUILL
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include "AdjustPosition1D.ipp"
+#include "AdjustPosition1I.ipp"
+#include "AdjustPosition1M.ipp"
+
+#include "AdjustPosition2D.ipp"
+#include "AdjustPosition2I.ipp"
+#include "AdjustPosition2M.ipp"
+
+#include "AdjustPosition3D.ipp"
+#include "AdjustPosition3I.ipp"
+#include "AdjustPosition3M.ipp"
+
+#include "AdjustPosition4D.ipp"
+#include "AdjustPosition4I.ipp"
+#include "AdjustPosition4M.ipp"
+
+#include "SetPosition1D.ipp"
+#include "SetPosition1I.ipp"
+#include "SetPosition1M.ipp"
+
+#include "SetPosition2D.ipp"
+#include "SetPosition2I.ipp"
+#include "SetPosition2M.ipp"
+
+#include "SetPosition3D.ipp"
+#include "SetPosition3I.ipp"
+#include "SetPosition3M.ipp"
+
+#include "SetPosition4D.ipp"
+#include "SetPosition4I.ipp"
+#include "SetPosition4M.ipp"

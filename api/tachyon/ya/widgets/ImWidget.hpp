2b968a11167b4a9ea59f9afb00d1c5adc80b012b
spaceape3001 <spaceape3001@users.noreply.github.com>
2025-03-14 19:54:41 +0000

    Moving Scenery to SDK
    Whether it belongs here or in exe, debatable, anyways, it'll be SDK
    example for a while

--- api/tachyon/ya/widgets/ImWidget.hpp
+++ api/tachyon/ya/widgets/ImWidget.hpp
@@ -10,13 +10,29 @@
 
 namespace yq::tachyon {
 
+    class ImWidgetInfo : public Widget²Info {
+    public:
+    
+        template <typename> class Writer;
+        
+        ImWidgetInfo(std::string_view, Widget²Info&, const std::source_location& sl = std::source_location::current());
+    
+    };
+
+
     /*! \brief A widget defined in ImGui that needs external 2D sizing
     
         This can be a toolbar itself, a floating window, etc...
     */
     class ImWidget : public Widget² {
+        YQ_TACHYON_INFO(ImWidgetInfo)
         YQ_TACHYON_DECLARE(ImWidget, Widget²)
     public:
+        
+        ImWidget();
+        ~ImWidget();
+        
+        static void init_info();
     
         //! The imgui wrapper
         virtual void    imgui(ViContext&) override final;

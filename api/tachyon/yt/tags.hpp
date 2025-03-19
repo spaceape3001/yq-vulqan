3966355220e3e09cf5beead497cdbfbb7d9330a0
spaceape3001 <spaceape3001@users.noreply.github.com>
2025-03-19 07:17:39 +0000

    Moving Tags
    Some tags defined in the toolbox

--- api/tachyon/yt/tags.hpp
+++ api/tachyon/yt/tags.hpp
@@ -6,10 +6,9 @@
 
 #pragma once
 
+#include <yq/tags.hpp>
+
 namespace yq::tachyon {
     //! Use this to make a property listable by a logger
     static constexpr const int kTag_Log     = 2000;
-    
-    //! Use this to capture state-save-data
-    static constexpr const int kTag_Save    = 2001;
 }

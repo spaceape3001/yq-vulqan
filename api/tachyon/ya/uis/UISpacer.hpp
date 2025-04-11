8c6299a47adc4be10f8d58fd44ac64108cca8004
spaceape3001 <spaceape3001@users.noreply.github.com>
2025-04-11 05:48:11 +0000

    Adding a spacer/empty UI element
    For those cases where a padding's necessary....

---
+++ api/tachyon/ya/uis/UISpacer.hpp
@@ -0,0 +1,42 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//  YOUR QUILL
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include <yt/ui/UIElement.hpp>
+#include <yq/shape/Size2.hpp>
+#include <yt/keywords.hpp>
+
+namespace yq::tachyon {
+
+    class UISpacerWriter;
+
+    /*! \brief UISpacer is a spacing device (with given dimensions)
+    */
+    class UISpacer : public UIElement {
+        YQ_OBJECT_DECLARE(UISpacer, UIElement)
+    public:
+        using Writer = UISpacerWriter;
+        
+        UISpacer(const Size2F& v=ZERO, UIFlags flags={});
+        UISpacer(const UISpacer&);
+        ~UISpacer();
+        
+        static void init_info();
+        
+        UISpacer* clone() const;
+        
+        const Size2F&     size() const { return m_size; }
+        void        size(set_k, const Size2F&);
+        
+    protected:
+    
+        void        render() override;
+        
+    private:
+        Size2F      m_size;
+    };
+}
---
+++ api/tachyon/ya/uis/UISpacerWriter.hpp
@@ -0,0 +1,24 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//  YOUR QUILL
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include <yt/ui/UIElementWriter.hpp>
+
+namespace yq::tachyon {
+    class UISpacer;
+    class UISpacerWriter : public UIElementWriter {
+    public:
+        UISpacerWriter();
+        UISpacerWriter(UISpacer*);
+        UISpacerWriter(const UISpacerWriter&);
+        ~UISpacerWriter();
+        
+        UISpacer* element();
+        
+    protected:
+    };
+}

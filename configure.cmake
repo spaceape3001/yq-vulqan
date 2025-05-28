a5082ff4702f54f9545ea40254d983c0520808c8
spaceape3001 <spaceape3001@users.noreply.github.com>
2025-05-28 21:06:35 +0000

    Renamed the macro

--- configure.cmake
+++ configure.cmake
@@ -13,7 +13,7 @@ if(NOT COMMAND vulkan_library)
     set(GLOBAL PROPERTY YQ_VULQAN_LIBRARIES)  
     
     #   Use this macro for non-tachyon libraries
-    macro(vulkan_library target)
+    macro(vulqan_library target)
         message("vulkan library ${target}")
         get_property(tmp GLOBAL PROPERTY YQ_VULQAN_LIBRARIES)
         #get_target_property(tgt ${target} LIBRARY_OUTPUT_NAME)
@@ -21,7 +21,7 @@ if(NOT COMMAND vulkan_library)
         set_property(GLOBAL PROPERTY YQ_VULQAN_LIBRARIES ${tmp})
     endmacro()
 
-    macro(vulkan_config)
+    macro(vulqan_config)
         if(${CMAKE_SOURCE_DIR} STREQUAL ${CMAKE_CURRENT_LIST_DIR})
             get_property(VULQAN_LIBARIES GLOBAL PROPERTY YQ_VULQAN_LIBRARIES)
             configure_file(${VULQAN_DIR}/vulqan.cfg.in ${CMAKE_BINARY_DIR}/bin/vulqan.cfg)

fc739c2ca012a1124650f48ce19fc78f995a168f
spaceape3001 <spaceape3001@users.noreply.github.com>
2025-06-02 06:06:19 +0000

    Adding Composite Scene

---
+++ api/tachyon/scene/CompositeScene.cpp
@@ -0,0 +1,26 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//  YOUR QUILL
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include "CompositeScene.hpp"
+#include <tachyon/api/SceneInfoWriter.hpp>
+
+YQ_TACHYON_IMPLEMENT(yq::tachyon::CompositeScene)
+
+namespace yq::tachyon {
+    CompositeScene::CompositeScene()
+    {
+    }
+    
+    CompositeScene::~CompositeScene()
+    {
+    }
+        
+    void CompositeScene::init_info()
+    {
+        auto w = writer<CompositeScene>();
+        w.description("Composite Scene");
+    }
+}
---
+++ api/tachyon/scene/CompositeScene.hpp
@@ -0,0 +1,26 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//  YOUR QUILL
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include <tachyon/api/Scene.hpp>
+
+namespace yq::tachyon {
+    /*! \brief A scene that's a composite of other scenes
+    
+        This will generally mean that this scene won't be directly renderable 
+        by the composite widget either.
+    */
+    class CompositeScene : public Scene {
+        YQ_TACHYON_DECLARE(CompositeScene, Scene)
+    public:
+    
+        CompositeScene();
+        ~CompositeScene();
+        
+        static void init_info();
+    };
+}

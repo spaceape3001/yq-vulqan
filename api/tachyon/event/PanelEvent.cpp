ac17fd3a11a3941448f17c4bcfb30d71f85a7321
spaceape3001 <spaceape3001@users.noreply.github.com>
2025-06-02 05:32:23 +0000

    Panel Event
    Adding panel events (kinda generic, meant for things of the UI "panel"
    variety)

---
+++ api/tachyon/event/PanelEvent.cpp
@@ -0,0 +1,31 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//  YOUR QPanelLL
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include <tachyon/event/PanelEvent.hpp>
+#include <tachyon/api/EventInfoWriter.hpp>
+
+YQ_OBJECT_IMPLEMENT(yq::tachyon::PanelEvent)
+
+namespace yq::tachyon {
+    PanelEvent::PanelEvent(const Header& h) : Event(h)
+    {
+    }
+    
+    PanelEvent::PanelEvent(const PanelEvent&cp, const Header&h) : Event(cp, h)
+    {
+    }
+
+    PanelEvent::~PanelEvent()
+    {
+    }
+    
+    void PanelEvent::init_info()
+    {
+        auto w = writer<PanelEvent>();
+        w.abstract();
+        w.description("Panel Event");
+    }
+}
---
+++ api/tachyon/event/PanelEvent.hpp
@@ -0,0 +1,31 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//  YOUR QPanelLL
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include <tachyon/api/Event.hpp>
+
+namespace yq::tachyon {
+
+    //! Instructs an object to set it's position
+    class PanelEvent : public Event {
+        YQ_OBJECT_DECLARE(PanelEvent, Event)
+    public:
+    
+        static void init_info();
+
+    protected:
+        PanelEvent(const Header&);
+        PanelEvent(const PanelEvent&, const Header&);
+        ~PanelEvent();
+    
+    private:
+        PanelEvent(const PanelEvent&) = delete;
+        PanelEvent(PanelEvent&&) = delete;
+        PanelEvent& operator=(const PanelEvent&) = delete;
+        PanelEvent& operator=(PanelEvent&&) = delete;
+    };
+}

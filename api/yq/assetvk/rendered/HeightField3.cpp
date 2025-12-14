1bb95a4f69757f688c535e354e66ffa51fbca7ce
spaceape3001 <spaceape3001@users.noreply.github.com>
2025-12-14 17:54:06 +0000

    Shader Cleanup

--- api/yq/assetvk/rendered/HeightField3.cpp
+++ api/yq/assetvk/rendered/HeightField3.cpp
@@ -6,6 +6,7 @@
 
 #include "HeightField3.hpp"
 
+#include <yq/color/colors.hpp>
 #include <yq/core/IntRange.hpp>
 #include <yq/math/UV.hpp>
 #include <yq/shape/AxBox2.hpp>
@@ -80,30 +81,8 @@ namespace yq::tachyon {
             //kRoleMaterialHMRay
         //};
             
-        static constexpr const Pipeline::Role   kDbgBlack       = Pipeline::Role::DbgBlack;
-        static constexpr const Pipeline::Role   kDbgRed         = Pipeline::Role::DbgRed;
-        static constexpr const Pipeline::Role   kDbgOrange      = Pipeline::Role::DbgOrange;
-        static constexpr const Pipeline::Role   kDbgYellow      = Pipeline::Role::DbgYellow;
-        static constexpr const Pipeline::Role   kDbgGreen       = Pipeline::Role::DbgGreen;
-        static constexpr const Pipeline::Role   kDbgCyan        = Pipeline::Role::DbgCyan;
-        static constexpr const Pipeline::Role   kDbgBlue        = Pipeline::Role::DbgBlue;
-        static constexpr const Pipeline::Role   kDbgMagenta     = Pipeline::Role::DbgMagenta;
-        static constexpr const Pipeline::Role   kDbgGray        = Pipeline::Role::DbgGray;
-        static constexpr const Pipeline::Role   kDbgWhite       = Pipeline::Role::DbgWhite;
-        
-        static constexpr const Pipeline::Role   kDbgBlackHM     = YQ_PIPELINE_ROLE;
-        static constexpr const Pipeline::Role   kDbgRedHM       = YQ_PIPELINE_ROLE;
-        static constexpr const Pipeline::Role   kDbgOrangeHM    = YQ_PIPELINE_ROLE;
-        static constexpr const Pipeline::Role   kDbgYellowHM    = YQ_PIPELINE_ROLE;
-        static constexpr const Pipeline::Role   kDbgGreenHM     = YQ_PIPELINE_ROLE;
-        static constexpr const Pipeline::Role   kDbgCyanHM      = YQ_PIPELINE_ROLE;
-        static constexpr const Pipeline::Role   kDbgBlueHM      = YQ_PIPELINE_ROLE;
-        static constexpr const Pipeline::Role   kDbgMagentaHM   = YQ_PIPELINE_ROLE;
-        static constexpr const Pipeline::Role   kDbgGrayHM      = YQ_PIPELINE_ROLE;
-        static constexpr const Pipeline::Role   kDbgWhiteHM     = YQ_PIPELINE_ROLE;
-        
+        static constexpr const Pipeline::Role   kDebug          = YQ_PIPELINE_ROLE;
         static constexpr const Pipeline::Role   kSimple         = YQ_PIPELINE_ROLE;
-        static constexpr const Pipeline::Role   kSimpleHM       = YQ_PIPELINE_ROLE;
 
     }
 
@@ -120,98 +99,25 @@ namespace yq::tachyon {
         AHeightField::init_meta(w);
         AMaterial::init_meta(w);
         ASize³::init_meta(w);
-        
-        #if 0
-        {
-            auto& p = w.pipeline();
-            p.shader( "yq/heightfield3/simple.vert" );
-            p.shader( "yq/heightfield3/simple.tesc" );
-            p.shader( "yq/heightfield3/simple.tese" );
-            p.shader( "yq/debug/color/cyan.frag" );
-            p.topology(Topology::PatchList);
-            p.vertex(&HeightField³::m_vbo, {.activity=DYNAMIC});
-            p.index(&HeightField³::m_index);
-            p.polygons(PolygonMode::Line);
-            p.uniform(&HeightField³::m_ubo, {.activity=DYNAMIC, .binding = kU_heightUniform});
-            p.push_full();
-            p.patch_control_points(4);
-        }
-        #endif
-        
-        static const struct {
-            Pipeline::Role  role;
-            bool            hm;
-            const char*     frag;
-        } kDebugPipes[] = {
-            { kDbgBlack,        false, "yq/debug/color/black.frag" },
-            { kDbgBlackHM,      true,  "yq/debug/color/black.frag" },
-            { kDbgRed,          false, "yq/debug/color/red.frag" },
-            { kDbgRedHM,        true,  "yq/debug/color/red.frag" },
-            { kDbgOrange,       false, "yq/debug/color/orange.frag" },
-            { kDbgOrangeHM,     true,  "yq/debug/color/orange.frag" },
-            { kDbgYellow,       false, "yq/debug/color/yellow.frag" },
-            { kDbgYellowHM,     true,  "yq/debug/color/yellow.frag" },
-            { kDbgGreen,        false, "yq/debug/color/green.frag" },
-            { kDbgGreenHM,      true,  "yq/debug/color/green.frag" },
-            { kDbgCyan,         false, "yq/debug/color/cyan.frag" },
-            { kDbgCyanHM,       true,  "yq/debug/color/cyan.frag" },
-            { kDbgBlue,         false, "yq/debug/color/blue.frag" },
-            { kDbgBlueHM,       true,  "yq/debug/color/blue.frag" },
-            { kDbgMagenta,      false, "yq/debug/color/magenta.frag" },
-            { kDbgMagentaHM,    true,  "yq/debug/color/magenta.frag" },
-            { kDbgGray,         false, "yq/debug/color/gray.frag" },
-            { kDbgGrayHM,       true,  "yq/debug/color/gray.frag" },
-            { kDbgWhite,        false, "yq/debug/color/white.frag" },
-            { kDbgWhiteHM,      true,  "yq/debug/color/white.frag" }
-        };
-
-        for(auto& k : kDebugPipes){
-            assert(!w.has_pipeline(k.role));
-        
-        
-            auto& p = w.pipeline(k.role);
-            p.shader( k.hm ? 
-                        "yq/heightfield3/heightmap.vert" :
-                        "yq/heightfield3/simple.vert" 
-            );
-            p.shader( "yq/heightfield3/simple.tesc" );
-            p.shader( k.hm ? 
-                        "yq/heightfield3/heightmap.tese" :
-                        "yq/heightfield3/simple.tese" 
-            );
-            p.shader( k.frag );
-            p.topology(Topology::PatchList);
-            p.vertex(&HeightField³::m_vbo, {.activity=DYNAMIC});
-            p.index(&HeightField³::m_index);
-            p.polygons(PolygonMode::Line);
-            p.uniform(&HeightField³::m_ubo, {.activity=DYNAMIC, .binding = kU_heightUniform});
-            if(k.hm){
-                p.texture(&HeightField³::m_heightMap, {.activity=DYNAMIC, .binding = kT_heightMap});
-            }
-            p.push_full();
-            p.patch_control_points(4);
-            
-        }
-
 
         {
-            auto& p = w.pipeline(kSimple);
-            p.shader( "yq/heightfield3/simple.vert" );
+            auto& p = w.pipeline(kDebug);
+            p.shader( "yq/heightfield3/heightmap.vert" );
             p.shader( "yq/heightfield3/simple.tesc" );
-            p.shader( "yq/heightfield3/simple.tese" );
+            p.shader( "yq/heightfield3/heightmap.tese" );
             p.shader( "yq/heightfield3/simple.frag" );
             p.topology(Topology::PatchList);
             p.vertex(&HeightField³::m_vbo, {.activity=DYNAMIC});
             p.index(&HeightField³::m_index);
-            p.polygons(PolygonMode::Fill);
+            p.polygons(PolygonMode::Line);
             p.uniform(&HeightField³::m_ubo, {.activity=DYNAMIC, .binding = kU_heightUniform});
+            p.texture(&HeightField³::m_heightMap, {.activity=DYNAMIC, .binding = kT_heightMap});
             p.push_full();
             p.patch_control_points(4);
         }
 
-
         {
-            auto& p = w.pipeline(kSimpleHM);
+            auto& p = w.pipeline(kSimple);
             p.shader( "yq/heightfield3/heightmap.vert" );
             p.shader( "yq/heightfield3/simple.tesc" );
             p.shader( "yq/heightfield3/heightmap.tese" );
@@ -392,47 +298,52 @@ namespace yq::tachyon {
         m_ubo.data.iTess = m_iTess.cast<float>();
         m_heightMap     = m_heightField ? m_heightField : sZeroHM;
 
-        tachyonInfo << "HeightField³::rebuild() ... height field? " << (m_heightMap ? "yes" : "no");
-        
         switch(m_drawMode){
-        case DrawMode::Auto:
-            set_pipeline(kDbgCyanHM);
-            break;
-            
         case DrawMode::Color:
             m_ubo.data.rgba  = m_color;
-            set_pipeline(kSimpleHM);
+            set_pipeline(kSimple);
             break;
         case DrawMode::DbgBlack:
-            set_pipeline(kDbgBlackHM);
+            m_ubo.data.rgba  = rgba4f(color::Black);
+            set_pipeline(kDebug);
             break;
         case DrawMode::DbgRed:
-            set_pipeline(kDbgRedHM);
+            m_ubo.data.rgba  = rgba4f(color::Red);
+            set_pipeline(kDebug);
             break;
         case DrawMode::DbgOrange:
-            set_pipeline(kDbgOrangeHM);
+        case DrawMode::Auto:
+            m_ubo.data.rgba  = rgba4f(color::Orange);
+            set_pipeline(kDebug);
             break;
         case DrawMode::DbgYellow:
-            set_pipeline(kDbgYellowHM);
+            m_ubo.data.rgba  = rgba4f(color::Yellow);
+            set_pipeline(kDebug);
             break;
         case DrawMode::DbgGreen:
-            set_pipeline(kDbgGreenHM);
+            m_ubo.data.rgba  = rgba4f(color::Green);
+            set_pipeline(kDebug);
             break;
-        default:
         case DrawMode::DbgCyan:
-            set_pipeline(kDbgCyanHM);
+            m_ubo.data.rgba  = rgba4f(color::Cyan);
+            set_pipeline(kDebug);
             break;
         case DrawMode::DbgBlue:
-            set_pipeline(kDbgBlueHM);
+            m_ubo.data.rgba  = rgba4f(color::Blue);
+            set_pipeline(kDebug);
             break;
         case DrawMode::DbgMagenta:
-            set_pipeline(kDbgMagentaHM);
+            m_ubo.data.rgba  = rgba4f(color::Magenta);
+            set_pipeline(kDebug);
             break;
         case DrawMode::DbgGray:
-            set_pipeline(kDbgGrayHM);
+        default:
+            m_ubo.data.rgba  = rgba4f(color::Gray);
+            set_pipeline(kDebug);
             break;
         case DrawMode::DbgWhite:
-            set_pipeline(kDbgWhiteHM);
+            m_ubo.data.rgba  = rgba4f(color::White);
+            set_pipeline(kDebug);
             break;
         }
 

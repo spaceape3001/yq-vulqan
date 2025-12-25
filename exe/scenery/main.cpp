fd13e887e75c5149d4f5b8530c164f157f187db8
spaceape3001 <spaceape3001@users.noreply.github.com>
2025-12-25 10:01:51 +0000

    Scenery Command Line Options

--- exe/scenery/main.cpp
+++ exe/scenery/main.cpp
@@ -14,6 +14,7 @@
 #include <yq/file/FileResolver.hpp>
 #include <yq/process/PluginLoader.hpp>
 #include <yq/tachyon/api/Tachyon.hxx>
+#include <yq/text/match.hpp>
 
 TypedID     gFileIO;
 
@@ -30,6 +31,23 @@ int main(int argc, char* argv[])
     aci.view.resizable      = true;
     //aci.view.depth_buffer   = ENABLE;
     
+    std::vector<std::filesystem::path>  imports, plugins, vlibs;
+    
+    for(int n=1;n<argc;++n){
+        if(argv[n][0] == '-'){
+            std::string_view    rem(argv[n]+2);
+            switch(argv[n][1]){
+            case 'R':
+                Resource::add_path(rem);
+                break;
+            case 'P':
+                plugins.push_back(rem);
+                break;
+            }
+        } else
+            imports.push_back(argv[n]);
+    }
+    
     Application app(argc, argv, aci);
     Meta::init();
     app.vulqan_libraries(LOAD);
@@ -40,8 +58,20 @@ int main(int argc, char* argv[])
     load_plugin_dir("plugin/luavk");
     #endif
     
+    for(auto& fp : plugins){
+        std::error_code ec;
+        if(std::filesystem::is_directory(fp, ec)){
+            load_plugin_dir(fp);
+        } else {
+            load_plugin(fp);
+        }
+    }
+    
     Meta::freeze();
     
+    //  TODO
+    //std::vector< ResourceTBD > open
+
     for(const std::filesystem::path& pth : Resource::all_paths())
         yInfo() << "resource path> " << pth;
     
@@ -50,13 +80,10 @@ int main(int argc, char* argv[])
     for(StdThread st : StdThread::all_values())
         yInfo() << "thread " << st.key() << "> " << Thread::standard(st).id;
     
-    //  TODO
-    //std::vector< ResourceTBD > open
-    
     gFileIO             = Tachyon::create_on<FileIOManager>(IO)->typed_id();
     SceneEditor* w      = Widget::create<SceneEditor>([&](SceneEditor& se){
-        for(int n=1;n<argc;++n)
-            se._import(argv[n]);
+        for(auto& fp : imports)
+            se._import(fp);
     });
     app.run(w);
     return 0;

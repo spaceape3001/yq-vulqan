de63d9863eadeb0abefc127310659562c0e13e11
spaceape3001 <spaceape3001@users.noreply.github.com>
2026-03-08 13:04:51 +0000

    XG Controller Work

--- api/yq/xgvk/api/XGContextVk.hpp
+++ api/yq/xgvk/api/XGContextVk.hpp
@@ -11,6 +11,16 @@
 namespace yq::tachyon {
     struct XGContextVk : public XGContext {
     
+        //! Use to finish tearing (US) down
+        bool    tear_finished    = false;
+
+        //! Use to start tearing down 
+        bool    tear_request     = false;
+        
+        //! Use to start ticking ... only valid during setup
+        bool    tick_request     = false;
+        
+    
         XGContextVk();
         virtual ~XGContextVk();
     };
